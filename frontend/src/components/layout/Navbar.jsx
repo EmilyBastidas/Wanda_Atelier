e328189886e9f070224b7430db422360a17b0293
Emily Bastidas <enily3@gmail.com>
2026-07-03 16:00:46 -0400

    feat: implementar navbar con routing y estilos b:wqase#:wq

---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,62 @@
+import { NavLink } from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
+import { FiShoppingBag } from "react-icons/fi";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-white border-bottom px-4">
+      <NavLink className="navbar-brand" to="/">
+        <img
+          src="/images/wanda_atelier.png"
+          alt="Wanda Atelier"
+          style={{ height: "40px" }}
+        />
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarContent"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarContent">
+        <ul className="navbar-nav mx-auto gap-3">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              INICIO
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/catalog">
+              COLECCIONES
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about">
+              NOSOTROS
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/contact">
+              CONTACTO
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className="d-flex align-items-center gap-3">
+          <button className="btn btn-link text-dark">
+            <FiSearch />
+          </button>
+
+          <button className="btn btn-link text-dark">
+            <FiShoppingBag />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
