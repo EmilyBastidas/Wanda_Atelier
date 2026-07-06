a76e60403aecff2cffb84d97a2e7f97d0871810c
Emily Bastidas <enily3@gmail.com>
2026-07-06 15:30:48 -0400

    Configura react-router-dom y añade las rutas iniciales con la barra de navegacion

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar/Navbar";
+import Navbar from "./components/layout/Navbar.jsx";
 
 function App() {
   return (
