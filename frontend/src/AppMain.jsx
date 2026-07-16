c9beca1be4a9373f75a516b53fcaaedef4085471
Emily Bastidas <enily3@gmail.com>
2026-07-15 22:47:47 -0400

    add rutas appMain.jsx

--- frontend/src/AppMain.jsx
+++ frontend/src/AppMain.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar.jsx";
 import Home from "./pages/Home.jsx";
+import Joyas from "./pages/Catalogo.jsx";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/catalogo" element={<h1>Catalogo</h1>} />
+        <Route path="/catalogo" element={<Joyas />} />
         <Route path="/nosotros" element={<h1>Nosotros</h1>} />
         <Route path="/contacto" element={<h1>Contacto</h1>} />
         <Route path="*" element={<h1>Página no encontrada (404)</h1>} />
